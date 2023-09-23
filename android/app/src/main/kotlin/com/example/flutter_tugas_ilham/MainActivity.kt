92d3f0e5386e2293ffc8624f6284c5e868412819
Muhammad Ilham <kircle24@gmail.com>
2023-09-23 13:49:14 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_tugas_ilham/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_tugas_ilham
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'MaterialApp'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff393E46),
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          size: 40, //change size on your need
+          color: Colors.white, //change color on your need
+        ),
+        backgroundColor: Color(0xFF222831),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Column(
+            children: <Widget>[
+              // Text(
+              //   'This is Materiall App',
+              //   style: TextStyle(color: Colors.white),
+              // ),
+              SizedBox(
+                height: 40,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const ListTile(
+                    leading: CircleAvatar(
+                      child: Text(
+                        "L",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      backgroundColor: Colors.green,
+                    ),
+                    title: Text(
+                      "dfsdfsd",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      "fdsfsdf",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      drawer: const Drawer(
+        backgroundColor: Color(0xFF222831),
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 26,
+            top: 46,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Home",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Setting", style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xff393E46),
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+      ),
+    );
+  }
+}
